c0cf019f86feb85af1e7a33573a9f6a18c3bc562
Jose O. Lara <jlarapr@gmail.com>
2015-11-08 20:00:44 -0400

    2015.11.8

--- WpfEndososCandidatos/WpfEndososCandidatos/Models/EndososPartidos.Designer.cs
+++ WpfEndososCandidatos/WpfEndososCandidatos/Models/EndososPartidos.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'E:\Source\Worspace2\EndososCandidatos2015\WpfEndososCandidatos\WpfEndososCandidatos\Models\EndososPartidos.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Source\Workspaces\EndososCandidatos2015\WpfEndososCandidatos\WpfEndososCandidatos\Models\EndososPartidos.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
